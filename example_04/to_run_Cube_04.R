cac58ce18600eacf39d6dd129785867525467176
Davide <davide.messina@ars.toscana.it>
2023-06-13 12:15:56 +0200

    Added example 04 based on DP3

---
+++ example_04/to_run_Cube_04.R
@@ -0,0 +1,42 @@
+#-------------------------------
+# Example 1: based on aggregated data, aggregate further, along 3 dimensions: Geography (3 levels), CalendarTime (2 levels), and Gender (2 levels)
+
+rm(list=ls(all.names=TRUE))
+
+# set the directory where the file is saved as the working directory
+if (!require("rstudioapi")) install.packages("rstudioapi")
+thisdir <- setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+thisdir <- setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
+dirinput <- paste0(thisdir,"/input/")
+diroutput <- paste0(thisdir,"/output/")
+
+# load data.table
+if (!require("data.table")) install.packages("data.table")
+library(data.table)
+
+#load function
+source(paste0(thisdir,"/../Cube.R"))
+
+#load input
+data_example <- fread(paste0(thisdir,"/input/data_example4.csv"), sep = ",")
+
+# USE THE FUNCTION 
+
+# assign the mandatory argument assigned_levels, containing the levels of each dimension
+
+assigned_levels <- vector(mode="list")
+assigned_levels[["Ageband"]] <- c("Ageband")
+
+# apply the function: note that the dimension Gender has its total computed, and that the statistics are not assigned, thus making Cube compute the default statistics (sum)
+
+output <- Cube(input = data_example,
+               dimensions = c("Ageband"),
+               levels = assigned_levels,
+               measures = c("prev_MS1", "prev_MS2", "prev_MS3", "prev_MS4", "prev_MS5", "in_population"),
+               computetotal = c("Ageband"),
+               summary_threshold = 100
+)
+
+View(output)
+fwrite(output,file=paste0(diroutput,"/output.csv"))
